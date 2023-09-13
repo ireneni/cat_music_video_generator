bb49881652d911a54501bcbdf4e3dd98d2de7d38
Irene Ni <rhn0312@gmail.com>
2023-09-13 15:34:12 +0200

    finishing touches and adding requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import urllib
 import json
 import requests
